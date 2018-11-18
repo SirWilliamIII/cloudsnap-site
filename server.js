7ae1b31963850c416c66ee9eec06e5fa315bdbf7
Will Carpenter <willcarpenteriii@gmail.com>
2018-11-18 02:19:36 -0600

    added dependency

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const express = require('express')
+const serveStatic = require('serve-static')
+const port = process.env.PORT || 5000
+
+app = express()
+app.use(serveStatic(__dirname + "/dist"))
+
+app.listen(port)
+console.log('server started '+ port)
+
+
