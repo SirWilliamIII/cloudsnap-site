2ea28047d632225a8ee3cc0438d752a405575860
Will Carpenter <willcarpenteriii@gmail.com>
2018-11-18 19:15:17 -0600

    Adding files.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-git pull origin master
-if [ $? -eq 0 ]
-then
 
+git pull origin master
+
+if [[ $? -eq 0 ]]
+then
 # read -p "Commit message: " commit
- git commit -am "_"
- git push heroku master
+    git commit -am "_"
+    git push heroku master
 fi
