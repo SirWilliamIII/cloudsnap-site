0c4029a89b4fd567973658b586252d584b858ced
Will Carpenter <willcarpenteriii@gmail.com>
2018-11-25 19:08:30 -0600

    redirect to thanks page on successful email

--- src/views/pages/ThanksPage.vue
+++ src/views/pages/ThanksPage.vue
@@ -3,7 +3,10 @@
 		<Navbar />
 		<div id="content">
 			<!-- TEMPLATE HERE-->
-
+			<div class="container" style="padding-top:100px;">
+				<h1 class="text-center" id="title">Thank You!</h1>
+				<p class="text-center" id="subtitle">We have received your message and will contact you shortly</p>
+			</div>
 			<!-- END -->
 		</div>
 		<Footer />
@@ -34,5 +37,34 @@
 
 	#content {
 		margin-top: 100px;
+	}
+
+	#title {
+		padding-top:    50px;
+		padding-bottom: 10px;
+		color:          #fff;
+		font-size:      94px;
+		font-weight:    300;
+	}
+
+	#subtitle {
+		color:       #fff;
+		font-size:   26px;
+		font-weight: 300;
+	}
+
+	@media only screen and (max-width: 768px) {
+
+		#title {
+			padding-top: 0;
+			font-size:   56px;
+			margin-top:  0;
+		}
+
+		#subtitle {
+			margin-top: 20px;
+			font-size: 22px;
+		}
+
 	}
 </style>
